f70a6ddf72a556f61d504490ec793065f7160d52
Justin Vittitoe <justinvittitoe@Justins-MacBook-Air-2.local>
2025-04-06 18:04:00 -0600

    authentication added to application

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,29 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch(
+      //Authorization endpoint and what type of request
+      '/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/JSON',
+        },
+        //send the user info as a JSON string to the endpoint
+        body: JSON.stringify(userInfo)
+      },
+    )
+    //await the approved authentication - JWT webtoken should be returned
+    const data = await response.json()
+    //Important to keep the error message generic
+    if(!response.ok) {
+      throw new Error('Invalid user login, please try again')
+    }
+    return data
+  } catch (err) {
+    console.log('Error with login')
+    return Promise.reject('Could Not login user')
+  }
 }
 
 
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,52 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
+import type { UserData } from '../interfaces/UserData';
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<UserData>(this.getToken())
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      //atempt to decode the token
+      const decoded = jwtDecode<JwtPayload>(token);
+      //check if the token has an expiration property
+      //check if the expiration is within an hour from when it was generate
+      if(decoded?.exp && decoded?.exp < Date.now() / 1000) {
+        //If both statements are truthy return true to the loggedIn function
+        return true
+      }
+    } catch (err) {
+      return false
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || "";
+    return loggedUser
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken); 
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/')
   }
 }
 
